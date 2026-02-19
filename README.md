fd555c52d63dadb797df5e296ec56a04f4e3a4f1
Paul <paguilar4@ewu.edu>
2026-02-19 02:51:21 +0000

    should have been included in previous commit

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # internet_censorship
 lab7 - implement 3 types of censorship
+
+
+Forward machine setup
+
+
+1) set up TPROXY 
+    
+    `sudo modprobe nft_tproxy`
+
+- can check set up with 
+    
+    `lsmod | grep tproxy`
+
+2) set up alice ip table rules
+ 
+    `sudo nft add table nat`
+
+    `sudo nft 'add chain ip nat POSTROUTING { type nat hook postrouting priority 100; }'`    
+    `sudo nft add rule ip nat POSTROUTING oifname "enp0s8" masquerade`
+
+3) run bash script
+
+4) run ip table rules for bash script
+
+5) run forwarding machine
--- censor.py
+++ censor.py
@@ -1,7 +1,10 @@
 #https://scapy.readthedocs.io/en/latest/advanced_usage/fwdmachine.html
 
+from scapy.all import *
+print("Loaded from:", scapy.__file__)
 from scapy.fwdmachine import ForwardMachine
 from scapy.layers.http import HTTP
+import socket
 
 class NOPFwdMachine(ForwardMachine):
     def xfrmcs(self, pkt, ctx):
@@ -12,17 +15,9 @@ class NOPFwdMachine(ForwardMachine):
         pkt.show()  # we print the server->client packets
         raise self.FORWARD()
 
-# Run it
 NOPFwdMachine(
     mode=ForwardMachine.MODE.TPROXY,
-    port=80,
-    cls=HTTP,  # we specify the class of the payload we are receiving
-).run()
-
-# Run it
-NOPFwdMachine(
-    mode=ForwardMachine.MODE.TPROXY,
-    port=443,
+    port=8080,
     cls=HTTP,
-    ssl=True,
+    af=socket.AF_INET
 ).run()
